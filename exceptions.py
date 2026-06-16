f78dcb0880ce468c09a86b5b36e648e2fa1cfe2f
HenriqueHartmann <alemaohartmann@gmail.com>
2026-06-15 23:33:20 -0300

    fix(exceptions): chain base Exception in ConfigFieldIsEmpty and MethodNotImplemented

--- exceptions.py
+++ exceptions.py
@@ -12,7 +12,24 @@ class ConfigFieldIsEmpty(Exception):
     def __init__(self, field: str):
         self.field = field
         self.message = f"{field} is empty"
+        super().__init__(self.message)
 
+
+class FileNotFound(Exception):
+    code = "FILE_NOT_FOUND"
+
+    def __init__(self, file_with_path: str):
+        self.file_with_path = file_with_path
+        self.message = f"File not found: {self.file_with_path}"
+        super().__init__(self.message)
+
+
+class FilePermissionError(Exception):
+    code = "FILE_PERMISSION_ERROR"
+
+    def __init__(self, file_with_path: str):
+        self.file_with_path = file_with_path
+        self.message = f"Permission denied for file: {self.file_with_path}"
         super().__init__(self.message)
 
 
@@ -21,3 +38,4 @@ class MethodNotImplemented(Exception):
 
     def __init__(self):
         self.message = "Method not implemented"
+        super().__init__(self.message)
